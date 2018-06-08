8a7d4193529584c7a331d49433b4bedae852f565
Nywton Barros <nywton.barros@gmail.com>
2018-06-07 21:53:06 -0300

    Search Bar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def search_bar(arg)
+    content_tag :div, class: 'input-group' do
+      concat(content_tag(:input,nil, type: 'text', class: 'form-control'))
+      concat(content_tag :span, content_tag(:button, 'Buscar', class: 'btn btn-primary'), class: 'input-group-append')
+    end
+  end
 end
